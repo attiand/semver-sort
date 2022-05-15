67128368ad3ba7b227761986349b08d8cdb4f20e
Mattias Andersson <mattias.j.andersson@gmail.com>
2022-05-15 16:47:52 +0200

    added --filter option

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # semver
-Print and optional sort lines that match a semantic version.
+Print, filter, sort lines that match a semantic version (https://semver.org)
+
+Internally uses https://crates.io/crates/semver for sorting and matching. Filter expressions syntax is described [here](https://docs.rs/semver/1.0.9/semver/struct.VersionReq.html#syntax)
 
 ```
-semver 2.0.0
-Print and optional sort lines that match a semantic version.
+semver 2.1.0
+Print, filter, sort lines that match a semantic version (https://semver.org).
 
 Print lines that match a semantic version to standard output. With no FILE, or when FILE is '-',
 read standard input.
@@ -19,14 +21,17 @@ OPTIONS:
         --completion
             Generate bash completion and exit
 
+    -f, --filter <EXPR>
+            Filter versions acording to expression. Has no meaning with --invert
+
     -h, --help
             Print help information
 
     -i, --invert
-            Invert match, print unrecognized lines
+            Invert match, i.e. print lines that not match a semantic version
 
     -r, --reverse
-            Sort lines, reversed order
+            Sort lines in reversed order
 
     -s, --sort
             Sort lines
@@ -37,3 +42,13 @@ OPTIONS:
     -V, --version
             Print version information
 ```
+
+## Examples
+
+Print tags that matches semantic version.
+
+> git tag | semver
+
+Print tags that has major version number 1. 
+
+> semver --filter '>= 1.0.0, <2.0.0' tags.txt
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-use semver::Version;
+use semver::{Version, VersionReq};
 
 use clap::{IntoApp, Parser};
 use clap_complete::{generate, shells::Bash};
+use clap_mangen;
 
 const STDIN_FILE_NAME: &str = "-";
 
-/// Print and optional sort lines that match a semantic version.
+/// Print, filter, sort lines that match a semantic version (https://semver.org).
 ///
 /// Print lines that match a semantic version to standard output. With no FILE, or when FILE is '-', read standard input.
 #[derive(Parser)]
@@ -17,24 +18,32 @@ const STDIN_FILE_NAME: &str = "-";
 
 struct Args {
     /// Sort lines
-    #[clap(short, long, takes_value = false)]
+    #[clap(short, long, takes_value(false))]
     sort: bool,
 
-    /// Sort lines, reversed order
-    #[clap(short, long, takes_value = false)]
+    /// Sort lines in reversed order
+    #[clap(short, long, takes_value(false))]
     reverse: bool,
 
     /// Removes repeated versions (implies --sort)
-    #[clap(short, long, takes_value = false)]
+    #[clap(short, long, takes_value(false))]
     uniq: bool,
 
-    /// Invert match, print unrecognized lines
-    #[clap(short, long, takes_value = false)]
+    /// Filter versions acording to expression. Has no meaning with --invert
+    #[clap(short, long, value_name("EXPR"))]
+    filter: Option<String>,
+
+    /// Invert match, i.e. print lines that not match a semantic version
+    #[clap(short, long, takes_value(false))]
     invert: bool,
 
     /// Generate bash completion and exit
-    #[clap(long, takes_value = false)]
+    #[clap(long, takes_value(false))]
     completion: bool,
+
+    /// Generate manual page
+    #[clap(long, hide(true), takes_value(false))]
+    manual: bool,
 
     /// Files to process, if '-' read standard input
     files: Vec<String>,
@@ -64,6 +73,21 @@ fn main() -> Result<(), String> {
         return Ok(());
     }
 
+    if args.manual {
+        return match clap_mangen::Man::new(command).render(&mut io::stdout()) {
+            Ok(()) => Ok(()),
+            Err(msg) => Err(msg.to_string()),
+        };
+    }
+
+    let requirement = match args.filter {
+        Some(f) => match VersionReq::parse(&f) {
+            Ok(requirement) => Some(requirement),
+            Err(e) => return Err(e.to_string()),
+        },
+        None => None,
+    };
+
     if args.files.is_empty() {
         args.files.push(STDIN_FILE_NAME.to_string())
     }
@@ -83,38 +107,38 @@ fn main() -> Result<(), String> {
             if let Ok(l) = line {
                 let version = Version::parse(&l);
                 match version {
-                    Ok(v) => {
-                        versions.push(VersionType::SemVersion(v));
-                    }
-                    Err(_) => {
-                        versions.push(VersionType::Unknown(l));
-                    }
+                    Ok(v) => versions.push(VersionType::SemVersion(v)),
+                    Err(_) => versions.push(VersionType::Unknown(l)),
                 }
             }
         }
     }
 
-    if args.invert {
-        versions.retain(|v|matches!(v, VersionType::Unknown(_)));
-    }
-    else {
-        versions.retain(|v|matches!(v, VersionType::SemVersion(_)));
+    if !args.invert {
+        match requirement {
+            Some(r) => {
+                versions.retain(|c| matches!(c, VersionType::SemVersion(v) if r.matches(&v)))
+            }
+            None => versions.retain(|c| matches!(c, VersionType::SemVersion(_))),
+        }
+    } else {
+        versions.retain(|c| matches!(c, VersionType::Unknown(_)))
     }
 
     if args.sort || args.uniq || args.reverse {
-        versions.sort();
+        versions.sort()
     }
 
     if args.uniq {
-        versions.dedup();
+        versions.dedup()
     }
 
     if args.reverse {
-        versions.reverse();
+        versions.reverse()
     }
 
     for version in versions.iter() {
-        println!("{}", version);
+        println!("{}", version)
     }
 
     Ok(())
